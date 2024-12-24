f9765a826648fd36265b7dfd2034deb8b7d8cca8
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2024-12-25 01:15:04 +0900

    [Add] コマンドを選ぶパネルとボタンを管理するクラスの追加

---
+++ Assets/Scripts/System/CommandPanelView.cs
@@ -0,0 +1,32 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// 攻撃、スキル、防御、アイテムの選択を行うCommandパネルを管理します
+/// </summary>
+public class CommandPanelView : MonoBehaviour
+{
+    [SerializeField] private Button _attackButton;
+    [SerializeField] private Button _skillButton;
+    [SerializeField] private Button _defendButton;
+    [SerializeField] private Button _itemButton;
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void Initialize(Action onAttack, Action onSkill, Action onDefend, Action onItem)
+    {
+        _attackButton.onClick.AddListener(() => onAttack());
+        _skillButton.onClick.AddListener(() => onSkill());
+        _defendButton.onClick.AddListener(() => onDefend());
+        _itemButton.onClick.AddListener(() => onItem());
+    }
+}
---
+++ Assets/Scripts/System/GameStateEnum.cs
@@ -0,0 +1,13 @@
+/// <summary>
+/// ゲームの状態の列挙型
+/// </summary>
+public enum GameStateEnum
+{
+    StartPerformance,
+    FirstSelect,
+    Chara1Select,
+    Chara2Select,
+    Chara3Select,
+    Chara4Select,
+    
+}
